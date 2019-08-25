e68eb233fceb4c7870af33408ba0732adc56a9fe
Jordan Michele <jordanmichele32@gmail.com>
2019-08-25 14:44:46 -0400

    broken homepage into smaller components, finished styling menuitem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Homepage from "../src/homepage.component";
+import Homepage from "../src/pages/homepage/homepage.component";
 
 function App() {
   return (
